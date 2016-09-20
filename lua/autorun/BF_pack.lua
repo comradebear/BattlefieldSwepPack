567b65c33a9d1c5be59abdde8d775c5982ac1d5f
comradebear <xxcreepyguyxx@gmail.com>
2016-09-20 14:32:08 -0700

    adding models, materials, sounds, etc.

---
+++ lua/autorun/BF_pack.lua
@@ -0,0 +1,212 @@
+--BF Weapon sounds-- 
+--M4A1--
+sound.Add({
+	name = 			"BF_M4A1.Single",
+	channel = 		CHAN_WEAPON,
+	volume = 		1.0,
+	sound = 		{ 
+						"wpn/bf4/m4a1/m4A1_Fire.wav"
+					}
+})
+sound.Add({
+	name = 			"BF_M4A1.Magin",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/m4a1/m4a1_magin.wav"
+})
+sound.Add({
+	name = 			"BF_M4A1.Magout",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/m4a1/m4a1_magout.wav"
+})
+sound.Add({
+	name = 			"BF_M4A1.boltrelease",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/m4a1/m4a1_boltrelease.wav"
+})
+sound.Add({
+	name = 			"Generic.Deploy",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/misc/deploy.wav"
+})
+sound.Add({
+	name = 			"Generic.Rattle",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 		{ 
+						"wpn/misc/rattle1.wav",
+						"wpn/misc/rattle2.wav",
+						"wpn/misc/rattle3.wav",
+						"wpn/misc/rattle4.wav"
+					}
+})
+
+--RFB--
+sound.Add({
+	name = 			"BF_RFB.Single",
+	channel = 		CHAN_WEAPON,
+	volume = 		1.0,
+	sound = 		{ 
+						"wpn/bf4/rfb/rfb_fire1.wav",
+						"wpn/bf4/rfb/rfb_fire2.wav",
+						"wpn/bf4/rfb/rfb_fire3.wav"
+					}
+})
+sound.Add({
+	name = 			"BF_RFB.Magin",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/rfb/rfb_magin.wav"
+})
+sound.Add({
+	name = 			"BF_RFB.Magout",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/rfb/rfb_magout.wav"
+})
+sound.Add({
+	name = 			"BF_RFB.boltback",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/rfb/rfb_boltback.wav"
+})
+sound.Add({
+	name = 			"BF_RFB.boltforward",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/rfb/rfb_boltfoward.wav"
+})
+
+--P08 Luger--
+sound.Add({
+	name = 			"BF_Luger.Single",
+	channel = 		CHAN_WEAPON,
+	volume = 		1.0,
+	sound = 		{ 
+						"wpn/bf1/p08luger/luger_fire1.wav",
+						"wpn/bf1/p08luger/luger_fire2.wav",
+						"wpn/bf1/p08luger/luger_fire3.wav",
+						"wpn/bf1/p08luger/luger_fire4.wav"
+					}
+})
+sound.Add({
+	name = 			"BF_Luger.Magin",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf1/p08luger/luger_magin.wav"
+})
+sound.Add({
+	name = 			"BF_Luger.Magout",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf1/p08luger/luger_magout.wav"
+})
+sound.Add({
+	name = 			"BF_Luger.boltback",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf1/p08luger/luger_slideback.wav"
+})
+sound.Add({
+	name = 			"BF_Luger.boltforward",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf1/p08luger/luger_slideforward.wav"
+})
+
+--SV-98--
+sound.Add({
+	name = 			"BF_SV98.Single",
+	channel = 		CHAN_WEAPON,
+	volume = 		1.0,
+	sound = 		{ 
+						"wpn/bf4/sv98/sv98-1.wav",
+						"wpn/bf4/sv98/sv98-2.wav",
+						"wpn/bf4/sv98/sv98-3.wav"
+					}
+})
+sound.Add({
+	name = 			"BF_SV98.Magin",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/sv98/sv98_MagIn.wav"
+})
+sound.Add({
+	name = 			"BF_SV98.Magout",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/sv98/sv98_MagOut.wav"
+})
+sound.Add({
+	name = 			"BF_SV98.boltup",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/sv98/SV98-BoltUp.wav"
+})
+sound.Add({
+	name = 			"BF_SV98.boltback",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/sv98/SV98-BoltBack.wav"
+})
+sound.Add({
+	name = 			"BF_SV98.boltforward",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/sv98/SV98-BoltForward.wav"
+})
+sound.Add({
+	name = 			"BF_SV98.boltdown",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/sv98/SV98-BoltDown.wav"
+})
+
+--FNH F2000--
+sound.Add({
+	name = 			"BF_F2000.Single",
+	channel = 		CHAN_WEAPON,
+	volume = 		1.0,
+	sound = 		{ 
+						"wpn/bf4/f2000/f2000-1.wav"
+					}
+})
+sound.Add({
+	name = 			"BF_F2000.Magin",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/F2000/f2000_magin.wav"
+})
+sound.Add({
+	name = 			"BF_F2000.Magout",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/F2000/f2000_magout.wav"
+})
+sound.Add({
+	name = 			"BF_F2000.boltup",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/F2000/SV98-BoltUp.wav"
+})
+sound.Add({
+	name = 			"BF_F2000.boltback",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/F2000/f2000_BoltPull.wav"
+})
+sound.Add({
+	name = 			"BF_F2000.boltforward",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/F2000/f2000_BoltRelease.wav"
+})
+sound.Add({
+	name = 			"BF_F2000.boltdown",
+	channel = 		CHAN_USER_BASE+10,
+	volume = 		1.0,
+	sound = 			"wpn/bf4/F2000/SV98-BoltDown.wav"
+})
